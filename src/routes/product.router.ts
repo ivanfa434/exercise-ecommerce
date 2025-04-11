4bdf111048c575e901e92a7e6da8fae0ea1a4711
ivanfa434 <ivanfahrudinaziz123@gmail.com>
2025-04-11 11:18:30 +0700

    feat: create product (#3)

---
+++ src/routes/product.router.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { createProductController } from "../controller/product.controller";
+import { verifyToken } from "../lib/jwt";
+import { verifyRole } from "../middlewares/role.middleware";
+
+const router = Router();
+
+router.post("/", verifyToken, verifyRole(["ADMIN"]), createProductController);
+
+export default router;
---
+++ src/services/product/create-product.service.ts
@@ -0,0 +1,19 @@
+import { Product } from "@prisma/client";
+import prisma from "../../config/prisma";
+import { ApiError } from "../../utils/api-error";
+
+export const createProductService = async (
+  body: Product,
+) => {
+  const existingProduct = await prisma.product.findFirst({
+    where: { name: body.name },
+  });
+  if (existingProduct) {
+    throw new ApiError("Product already exists", 400);
+  }
+
+  const newProduct = await prisma.product.create({
+    data: body,
+  });
+  return newProduct;
+};
